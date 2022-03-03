ba57e06c84a735a7d17bd4b1e562d297e3827377
whoisarjen <kamilow97@gmail.com>
2022-03-03 02:23:10 +0100

    v

--- client/src/layout/footer/index.tsx
+++ client/src/layout/footer/index.tsx
@@ -1,28 +1,6 @@
-import styled from "styled-components";
-
-const FooterGrid = styled.footer`
-    width: 100%;
-    position: fixed;
-    bottom: 0;
-    height: 40px;
-    line-height: 40px;
-    display: grid;
-    font-size: 0.875rem;
-`
-
-const Placeholder = styled.div`
-    width: 100%;
-    height: 40px;
-`
-
 const Footer = () => {
     return (
-        <>
-            <Placeholder />
-            <FooterGrid>
-                Made by Kamil Owczarek
-            </FooterGrid>
-        </>
+        <></>
     )
 }
 
